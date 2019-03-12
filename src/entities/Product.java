5eb145ac83f3579dc8d0cad0de0df848eca5ff87
Ricardo Augusto <ricardo.a.barbosa@outlook.com>
2019-03-12 12:32:35 -0300

    Exercicio Proposto File

---
+++ src/entities/Product.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Product {
+	private String nome;
+	private double preco;
+	private Integer quantidade;
+
+	public Product() {
+	}
+
+	public Product(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double precoTotal() {
+		return preco * quantidade;
+	}
+}
